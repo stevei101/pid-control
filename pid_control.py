78548b790088bbef357b2037a0f7a3bc3187917f
stevei101 <steven.irvin@gmail.com>
2015-08-06 12:30:12 -0700

    WIP: add restart

--- pid_control.py
+++ pid_control.py
@@ -26,9 +26,21 @@ def pid_control(pids):
         except OSError:
             logger.error('ERROR: Could not kill PID {0}'.format(process_id))
 
+def pid_control_restart(pids):
+    """
+        control processes in unix like systems by PID
+    """
+    for process_id in pids:
+        try:
+            p = psutil.Process(int(process_id))
+            p.terminate()  #or p.kill()
+            logger.info('Stopped process PID {0}'.format(process_id))
+        except OSError:
+            logger.error('ERROR: Could not kill PID {0}'.format(process_id))
+
 def get_pids():
     """
-        get PIDs for processes on Unix like systems.
+        get PIDs for processes on Unix like systems and output them to a file.
     """
     try:
         pids = os.system('./list_pids.sh')
@@ -41,10 +53,22 @@ def get_pids():
     print('Found PID list {0}'.format(', '.join(pid_list)))
     return pid_list
 
+def retrieve_pids():
+    """
+        retrieve PIDs for processes on Unix like systems.
+    """
+    with open ("ceilometer.pid", 'rt') as pid_file:
+        pid_list = []
+        for process_id in pid_file.read().splitlines():
+            pid_list.append(process_id)
+    print('Found PID list {0}'.format(', '.join(pid_list)))
+    return pid_list
 
 if __name__ == "__main__":
-
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
     logger = logging.getLogger('pid_control')
-    pid_control(get_pids())
+    if sys.argv[1] is None:
+        pid_control(get_pids())
+    else:
+        pid_control_restart(retrieve_pids())
