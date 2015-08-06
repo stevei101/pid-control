ca8a983578fd645dbd994aeb23f8560a51039412
stevei101 <steven.irvin@gmail.com>
2015-08-06 11:25:34 -0700

    update readme

--- pid_control.py
+++ pid_control.py
@@ -9,6 +9,8 @@ import psutil
 """
    pid_control.py A Python tool to control processes in Unix like systems by PID.
    Aug, 2015 Steven D Irvin 
+
+   WARNING: You need to install psutil on the system and run this as $ sudo python pid_control.py
 """
 
 
