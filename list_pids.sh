af8f45875905d4b7be08b74221ad79fefb985387
stevei101 <steven.irvin@gmail.com>
2015-08-11 16:09:17 -0700

    fix ceilometer processes path

--- list_pids.sh
+++ list_pids.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps -ef | grep '/usr/bin/ceilometer' | grep -v grep | awk '{print $2 > "ceilometer.pid"}'
-ps -ef | grep '/usr/bin/ceilometer' | grep -v grep | awk '{print $9 > "ceilometer.processes"}'
+ps -ef | grep '/usr/local/bin/ceilometer' | grep -v grep | awk '{print $2 > "ceilometer.pid"}'
+ps -ef | grep '/usr/local/bin/ceilometer' | grep -v grep | awk '{print $9 > "ceilometer.processes"}'
