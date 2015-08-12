7e4ff4b18e841e07ef99062f33453aa028f1634a
stevei101 <steven.irvin@gmail.com>
2015-08-11 18:31:05 -0700

    update docs

--- process_list_restart.py
+++ process_list_restart.py
@@ -9,7 +9,7 @@ import shlex
 
 
 """
-   pid_control.py A Python tool to control processes in Unix like systems by PID.
+   process_list_restart.py A Python tool to control processes in Unix like systems by PID.
    Aug, 2015 Steven D Irvin 
 
    WARNING: You need to install psutil on the system and run this as $ sudo python pid_control.py
