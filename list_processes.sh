8acb1fc213f6bf3aa32e9178db8f0ff77038268b
stevei101 <steven.irvin@gmail.com>
2015-08-12 08:47:15 -0700

    expand processes using grep "celioemeter-"

--- list_processes.sh
+++ list_processes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps -ef | grep '/usr/local/bin/ceilometer' | grep -v grep | awk '{print $2 > "ceilometer.pid"}'
-ps -ef | grep '/usr/local/bin/ceilometer' | grep -v grep | awk '{$1=$2=$3=$4=$5=$6=$7=""; print $0 > "ceilometer.processes"}'
+ps -ef | grep 'ceilometer-' | grep -v grep | awk '{print $2 > "ceilometer.pid"}'
+ps -ef | grep 'ceilometer-' | grep -v grep | awk '{$1=$2=$3=$4=$5=$6=$7=""; print $0 > "ceilometer.processes"}'
